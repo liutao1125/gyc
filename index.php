5a1a3a8cf01a7459c878cd11ed87d09468c23934
Yang Chen <cy18140671773@outlook.com>
2016-10-28 15:03:26 +0800

    support 443 or 80

--- index.php
+++ index.php
@@ -18,18 +18,6 @@ define('APP_PATH', __DIR__ . '/app/');
 
 
 /**
- * 定义资源路径
- */
-define('ASSET_PATH', 'http://' . $_SERVER ['HTTP_HOST'] . substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php')) . 'asset/');
-
-
-/**
- * 定义根url(只有它最后没有‘/’)
- */
-define('BASE_URL', 'http://' . $_SERVER ['HTTP_HOST'] . rtrim(substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php')), '/'));
-
-
-/**
  * 定义框架核心路径
  */
 define('SYS_PATH', __DIR__ . '/gyc/');
@@ -39,6 +27,29 @@ define('SYS_PATH', __DIR__ . '/gyc/');
  * 定义上传文件夹路径
  */
 define('UPLOAD_PATH', __DIR__ . '/asset/upload/');
+
+
+/**
+ * 决定使用http或https
+ */
+if ($_SERVER['SERVER_PORT'] == '80') {
+    $transfer_protocol = 'http://';
+} elseif ($_SERVER['SERVER_PORT'] == '443') {
+    $transfer_protocol = 'https://';
+}
+
+
+/**
+ * 定义资源路径
+ */
+define('ASSET_PATH', "$transfer_protocol{$_SERVER ['HTTP_HOST']}" . substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php')) . 'asset/');
+
+
+/**
+ * 定义根url(只有它最后没有‘/’)
+ */
+define('BASE_URL', "$transfer_protocol{$_SERVER ['HTTP_HOST']}" . rtrim(substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], 'index.php')), '/'));
+
 
 /**
  * 引入核心框架文件
